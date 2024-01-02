81efc232956d9237703cca83a1437028abc5a077
souchardani <114416167+souchardani@users.noreply.github.com>
2024-01-02 17:00:37 +0100

    final seccion 8

---
+++ section8/section8-app2/src/components/KnowledgeGrid.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  //solo se puede inject lo que ha sido provide por un componente de nivel superior
+  //provide and inject necesitan una relacion ancestro-hijo o padre-hijo
+  inject: ['topics'],
+};
+</script>
