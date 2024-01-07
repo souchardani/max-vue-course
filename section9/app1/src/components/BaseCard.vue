a87a2a2b5e5e6c1cd087fcbb3235f838929d50f3
souchardani <114416167+souchardani@users.noreply.github.com>
2024-01-07 01:49:22 +0100

    finalizado: section8/114

---
+++ section9/app1/src/components/BaseCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header>
+      <!-- cuando añadimos mas de un slot, debemos identificarlos con el atributo name -->
+      <slot name="header">
+        <!-- todo el contenido que va dentro del slot es el contenido default que se va a sobreescribir -->
+        <!-- <h2>The Default</h2> -->
+      </slot>
+    </header>
+    <!-- solo puedes dejar uno sin nombrar, que va a ser el slot por defecto -->
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
+
+<script setup></script>
