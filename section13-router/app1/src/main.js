4a9414a205237a1dc7e96a054ab52cb9def9965f
souchardani <114416167+souchardani@users.noreply.github.com>
2024-01-30 22:47:01 +0100

    section router

---
+++ section13-router/app1/src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+
+import App from './App.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    //en rutas pasamos objetos, y cada objeto representa una ruta
+    {
+      path: '/teams',
+      component: TeamsList,
+    },
+    {
+      path: '/users',
+      component: UsersList,
+    },
+  ],
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
